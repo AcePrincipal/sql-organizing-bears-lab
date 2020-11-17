27c508e01d8c2b487a2f6e2029aa04b04873143b
Alexander <acepetrov96@gmail.com>
2020-11-16 18:16:25 -0600

    commit tree 6d2119ffbf57438ae95850dfd198896669c255fd

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('A', 3, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('B', 3, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('C', 3, 'F', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('D', 4, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('E', 5, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('F', 6, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('G', 1, 'M', 'Green', 'Chill', 'Y');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 3, 'M', 'Green', 'Mad', 'Y');
